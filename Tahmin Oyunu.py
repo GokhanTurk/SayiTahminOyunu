3d0ac98afe1dce15ba6b93ef22f64cf59a8bb575
GokhanTurk <ensar.gokhan@hotmail.com>
2022-03-08 11:11:16 +0300

    Gökhan Türk
    Kod okunulurluğu için düzenlemeler.

--- Tahmin Oyunu.py
+++ Tahmin Oyunu.py
@@ -7,45 +7,29 @@ rekortemp2= ""
 def clear(): #Ekran temizleme fonksiyonu
     system("cls")
 def susle(diziAd): #Sayıyı bildiğimizde ekrana gelen yazının uzunluğu kadar '-' ekler
-    s = 0
-    for i in diziAd:
-        s+=len(i)
-    for i in range(s):
-        print("-",end="")
+    uzunluk = 0
+    for i in diziAd: uzunluk+=len(i)
+    for i in range(uzunluk): print("-",end="")
     print("\n")
 def susle2(metin): # Daha büyük veya daha küçük ifadelerinin uzunluğu kadar ?. TAHMİN ifadesinin sağına ve soluna uygun şekilde paylaştırarak '-' işareti ekler
-    print("\n")
-    bk = str(tahmin) +". TAHMİN"
-    m = metin
-    final = ""
-    bkuzunluk = len(str(tahmin)) + 8
-    z = (len(m) - bkuzunluk) / 2
-    for i in range(int(z)):
-        final += "-"
-    final += bk
-    for i in range(int(z)):
-        final+="-"
-    if len(str(tahmin))%2 != 1:
-        final+="-"
-    final += "\n"
-    print(final)
-    print(m)
+    ortaCumle = str(tahmin) +". TAHMİN"
+    total = ""
+    yarimTire = (len(metin) - len(ortaCumle)) / 2
+    for i in range(int(yarimTire)): total += "-"
+    total += ortaCumle
+    for i in range(int(yarimTire)): total+="-"
+    if len(str(tahmin))%2 == 0: total+="-" 
+    print("\n" +total + "\n\n" + metin)
 def anyKey(yazdir): #Herhangi bir tuşla döngüden çıkmayı sağlayan fonksiyon
     while True:
         print(yazdir)
-        a = input("""
+        anykey = input("""
 
 Ana menüye dönmek için "Enter" tuşuna basınız! """)
-        if(a == "asqwe2"):
-            menuLogo()
-            clear()
-            system("color b")
-            break
-        else: 
-            clear()
-            menuLogo()
-            system("color b")
-            break
+        clear()
+        menuLogo()
+        system("color b")
+        break
 def logo(): #Logo yazdıran fonksiyon
     print("""
        o   o
@@ -76,7 +60,6 @@ def menuLogo(): #Menü logosunu yazdıran fonksiyon
           \/_____/   \/_____/   \/_____/   \/_/ \/_/   \/_____/                 
                                                                                                                                                         
         """)
-
 system("color b")
 clear()
 menuLogo() 
@@ -90,22 +73,22 @@ with open("TerstenRekor.txt","r") as Rekor2:   #Tersten Rekor değerini txt dosy
 Rekor2.close()
 
 while True: #Ana menü döngüsü
-    a = input("""
+    secenek = input("""
     Yeni Oyun : 1
     Rekorlar  : 2
     Yardım    : 3
     Çıkış     : q
     
     Giriniz   : """)
-    if(a == "1"): #Yeni Oyunu başlatır.
+    if(secenek == "1"): #Yeni Oyunu başlatır.
         clear()
         logo()
         UretilenSayi = random.randint(0,1000)
         while True: #Oyun döngüsü.    
             tahmin += 1
-            sayi =input("\nGizemli sayıyı Tahmin Edin :) = ")
-            if sayi.isdigit() and sayi != "AEZAKMI" and 0 <= int(sayi) <= 1000 and sayi !="q":  #Sayının belirlediğim aralıkta ve tipte girilmesini sağlıyorum. Hile ve çıkış için girdi kontrolü yapıyorum.
-                girilensayi = int(sayi)
+            girdi = input("\nGizemli sayıyı Tahmin Edin :) = ")
+            if girdi.isdigit() and girdi != "AEZAKMI" and 0 <= int(girdi) <= 1000 and girdi !="q":  #Sayının belirlediğim aralıkta ve tipte girilmesini sağlıyorum. Hile ve çıkış için girdi kontrolü yapıyorum.
+                girilensayi = int(girdi)
                 tahminstr = str(tahmin)
                 if girilensayi == UretilenSayi: #Girilen sayının üretilen sayıya eşit olması durumu.
                     if tahmin < int(rekortemp):     #Rekor kontrolü ve txt dosyasının üzerine yazma.
@@ -116,8 +99,7 @@ while True: #Ana menü döngüsü
                             sonuc = "Tebrikler rekor kırdınız :) | Tahmin: " + str(tahmin) + " Rekor: " + str(rekortemp) + " Tersten Rekor: " + str(rekortemp2)
                             print("\n")
                             susle(sonuc)
-                            for i in range(10):
-                                print(sonuc,"\n")
+                            for i in range(10): print(sonuc,"\n") 
                             susle(sonuc)
                             tahmin = 0
                     elif tahmin > int(rekortemp2): #Tersten rekor kontrolü ve txt dosyasının üzerine yazma.
@@ -128,8 +110,7 @@ while True: #Ana menü döngüsü
                             print("\n")   
                             sonuc = "Eh bu da rekor sayılır ama tersten :) | Tahmin: " + str(tahmin) + " Rekor: " + str(rekortemp) + " Tersten Rekor: " + str(rekortemp2)
                             susle(sonuc)
-                            for i in range(10):
-                                print(sonuc,"\n")
+                            for i in range(10): print(sonuc,"\n")     
                             susle(sonuc)
                             tahmin = 0
                     else:   #Her iki rekor da kırılmazsa gerçekleşecek durum.
@@ -137,8 +118,7 @@ while True: #Ana menü döngüsü
                         sonuc = "Tebrikler! | Tahmin: " + str(tahmin) + " Rekor: " + str(rekortemp) + " Tersten Rekor: " + str(rekortemp2)
                         print("\n")
                         susle(sonuc)
-                        for i in range(10):
-                                print(sonuc,"\n")
+                        for i in range(10): print(sonuc,"\n")       
                         susle(sonuc)
                         tahmin = 0
                     Rekor.close()
@@ -151,13 +131,12 @@ while True: #Ana menü döngüsü
                 elif girilensayi > UretilenSayi: #Girilen sayının üretilen sayıdan büyük olması durumu.
                     system("color c")
                     susle2("Daha küçük :)")
-                else:
-                    break
-            elif sayi == "AEZAKMI":  #Hile kodu, üretilen sayıyı gösterir.
+                else: break               
+            elif girdi == "AEZAKMI":  #Hile kodu, üretilen sayıyı gösterir.
                 system("color d")
                 tahmin-=1
                 print("\nHile yapmıyoruz değil mi :)) :",UretilenSayi)
-            elif sayi == "q": #Oyun döngüsünden çıkış sağlar.
+            elif girdi == "q": #Oyun döngüsünden çıkış sağlar.
                 tahmin = 0
                 clear()
                 menuLogo()
@@ -167,26 +146,22 @@ while True: #Ana menü döngüsü
                 system("color 4")
                 tahmin-=1
                 print("\nSadece 0 ile 1000 arasında bir tam sayı girebilirsiniz!")
-
-    elif(a == "2"): #Rekor listesi
+    elif(secenek == "2"): #Rekor listesi
         clear()
         logo()
         system("color a")
-        yazdir = "Rekor: " + str(rekortemp) + " Tersten Rekor: " + str(rekortemp2)
-        anyKey(yazdir)
-    elif(a == "3"): #Yardım bölümü
+        anyKey("Rekor: " + str(rekortemp) + " Tersten Rekor: " + str(rekortemp2))
+    elif(secenek == "3"): #Yardım bölümü
         clear()
         system("color 3")
         logo()
-        yazdir = "\n  -0 ile 1000 arasında sayı girişi yaparak gizemli sayıyı bulmaya çalışın.\n\n  -Oyun içindeyken, çıkmak için 'q' tuşuna basabilirsiniz."
-        anyKey(yazdir)
-    elif(a == "q"): #Çıkış
+        anyKey("\n  -0 ile 1000 arasında sayı girişi yaparak gizemli sayıyı bulmaya çalışın.\n\n  -Oyun içindeyken, çıkmak için 'q' tuşuna basabilirsiniz.")
+    elif(secenek == "q"): #Çıkış
         clear()
         print("Güle Güle!")
         break
-    else:           #Hatalı giriş.
+    else:   #Hatalı giriş.
         clear()
         logo()
         system("color 4")
-        yazdir = "Hatalı Giriş!"
-        anyKey(yazdir)
+        anyKey("Hatalı Giriş!")
